26a8a8310415854c596ce3fab8f1161d5ddda082
rahulkb007 <rahulkb007@gmail.com>
2017-01-14 09:26:16 +0530

    Added sample example

---
+++ core-java-concept/src/main/java/com/bhartiya/design/pattern/behavioral/chainofresponsibility/CheckAuthority.java
@@ -0,0 +1,10 @@
+package com.bhartiya.design.pattern.behavioral.chainofresponsibility;
+
+/**
+ * Created by Rahul on 14-01-2017.
+ */
+public class CheckAuthority {
+    public static void main(String[] args) {
+
+    }
+}
